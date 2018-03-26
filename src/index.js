4fe069e6deaac92322140b4fe1fba3cc9a9ff076
Bryan Campbell <bryanjcampbell1@gmail.com>
2018-03-26 10:37:01 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Web3 from 'web3';
+
+var $ = require('jquery');
+
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+//web3.eth.getAccounts().then(console.log);
+web3.eth.getAccounts(function(error, result) {
+    if(error != null)
+        console.log("Couldnt get accounts");
+   //console.log(result[0]);
+   console.log(result[0]);
+
+   //get balance of current account
+   web3.eth.getBalance(result[0], function(error, result){
+       if(!error)
+           console.log(JSON.stringify(result));
+       else
+           console.error(error);
+   });
+
+});
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
